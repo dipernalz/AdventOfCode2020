ee744e1dce034a5906e7adefe0e326c08e75810e
Leo DiPerna <lzdiperna@gmail.com>
2020-12-13 01:29:24 -0500

    Improved day 13

--- day13/part2.py
+++ day13/part2.py
@@ -1,13 +1,12 @@
 inpt = open('input.txt', 'r').read().split('\n')[1].split(',')
 
-inptLst = []
+n, incr = 0, -1
 for i in range(len(inpt)):
 	if inpt[i] != 'x':
-		inptLst.append((i, int(inpt[i])))
-searchLst, n, addend = [inptLst[0]], 0, 1
-for i in range(1, len(inptLst)):
-	searchLst.append(inptLst[i])
-	addend *= searchLst[-2][1]
-	while (n + searchLst[-1][0]) % searchLst[-1][1] != 0:
-		n += addend
+		if incr == -1:
+			incr = int(inpt[i])
+			continue
+		while (n + i) % int(inpt[i]) != 0:
+			n += incr
+		incr *= int(inpt[i])
 print(n)
